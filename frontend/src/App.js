f164ef1382802a6c9f7298d177d13f0ae0d0fa8a
Nina Weiler <ninaweiler3@gmail.com>
2021-07-24 14:09:06 +0300

    cleanup unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,10 @@ import vaccinationService from './services/vaccinations'
 import orderService from './services/orders'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-//import ArrivedToday from './components/ArrivedToday'
-import Calendar from './pages/Calendar'
+//import Calendar from './pages/Calendar'
 
 //Last order arrived on 2021-04-12 ?
-//First order arrived on 
+//First order arrived on 2021-01-02
 //First vaccination on 2021-01-02
 //Last vaccination on 2021-04-12
 
@@ -16,10 +15,7 @@ import Calendar from './pages/Calendar'
 const App = () => {
   const [vaccinations, setVaccinations] = useState([])
   const [orders, setOrders] = useState([])
-  //const [showDay, setShowDay] = useState([])
   const [loading, setLoading] = useState(false)
-  //const [combined, setCombined] = useState([])
-  //const [load, setLoad] = useState(false)
 
   
   useEffect(() => {
@@ -35,29 +31,7 @@ const App = () => {
   }, []) 
 
   
-  /*
-  useEffect(() => {
-    async function fetchData() {
-      setLoad(true)
-      const orderData = await orderService.getAll()
-      setOrders(orderData)
-      const data = await orders.map(o => orderService.getCombinedInfo(o.id))
-      //const data = await orderService.getCombinedInfo(orderData[7].id)
-      console.log('data', data[7])
-      //setCombined(data)
-      setLoad(false)
-    }
-    fetchData()
-  }, []) 
   
-
-  const showSelectedDay = (day) => {
-    const selectedVaccinations = vaccinations.filter(v => v.vaccinationDate.startsWith(day))
-    setShowDay(selectedVaccinations)
-  }
-  */
-  //console.log('order obect', orders[8])
-  //console.log('order vaccies', orders[8].vaccinations)
 
   return (
     <>
@@ -65,12 +39,6 @@ const App = () => {
     {/*{loading ? <p>Loading data..</p> : null}
     {load ? <p>Fetching combined data</p> : null} */}
     <Home vaccinations={vaccinations} orders={orders}/> 
-    {/*<Calendar vaccinations={vaccinations} orders={orders}/>
-    <button onClick={() => showSelectedDay('2021-03-07')}>Click</button>
-    {showDay.map((vaccination) => (
-      <p>{vaccination.gender}</p>
-    ))
-    } */}
     </>
   );
 }
