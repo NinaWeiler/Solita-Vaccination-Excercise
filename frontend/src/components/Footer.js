973b9ab276ac7483314314fef10d27bac440e261
Nina Weiler <ninaweiler3@gmail.com>
2021-08-06 18:40:28 +0300

    Added logo, styling navbar and footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import './Styles.css'
 const Footer = () => {
     return (
         <footer class="footer" >
-        <div class="content has-text-centered">
+        <div class="content has-text-centered has-text-danger-dark">
             <p>
             <strong>Vaxxy</strong> by <a href="https://github.com/NinaWeiler">Nina Weiler</a>.
             </p>
