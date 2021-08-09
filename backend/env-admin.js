621ddfc7021d347169b2bc3a3303cc084f0c74fe
Nina Weiler <ninaweiler3@gmail.com>
2021-08-09 17:09:00 +0300

    Removed file

--- backend/env-admin.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-undef */
-MONGODB_URI='mongodb+srv://admin-user:v3XfPmuWwWKDWwMy@cluster0.phqz3.mongodb.net/Vaccine-data?retryWrites=true&w=majority'
-
-TEST_MONGODB_URI='mongodb+srv://admin-user:v3XfPmuWwWKDWwMy@cluster0.phqz3.mongodb.net/test-vaccine-data?retryWrites=true&w=majority'
