b6ecf51de5c9db60b89cf54a1ccf472697654545
Nina Weiler <ninaweiler3@gmail.com>
2021-08-09 17:20:50 +0300

    Removed logging connection string

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 .then(() => {
