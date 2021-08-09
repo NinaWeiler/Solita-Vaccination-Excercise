c1644c7d16a4076cd62da7dc1d0c97f558769471
Nina Weiler <ninaweiler3@gmail.com>
2021-08-09 17:07:26 +0300

    New build

---
+++ backend/env-admin.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+MONGODB_URI='mongodb+srv://admin-user:v3XfPmuWwWKDWwMy@cluster0.phqz3.mongodb.net/Vaccine-data?retryWrites=true&w=majority'
+
+TEST_MONGODB_URI='mongodb+srv://admin-user:v3XfPmuWwWKDWwMy@cluster0.phqz3.mongodb.net/test-vaccine-data?retryWrites=true&w=majority'
